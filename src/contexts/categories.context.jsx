459e3fe37450cfd9fc524bcb7404677b3a8ea2cf
rasvanB <not.obelix@gmail.com>
2022-06-05 18:53:02 +0300

    set up graphql/apollo+ query for categories

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,23 +1,27 @@
-import { createContext, useState, useEffect } from 'react';
-
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
-
+import { createContext, useState, useEffect } from "react";
+import { gql } from "@apollo/client/core";
+import { useQuery } from "@apollo/client/react";
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
-
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 export const CategoriesProvider = ({ children }) => {
   const [categoriesMap, setCategoriesMap] = useState({});
-
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
-  }, []);
-
+  const { data, loading, error } = useQuery(COLLECTIONS);
+  console.log(data);
   const value = { categoriesMap };
   return (
     <CategoriesContext.Provider value={value}>
