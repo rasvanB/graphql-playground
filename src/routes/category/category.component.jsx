1bf59294db1fa5986e22d622bed8b1ea3a29fff9
rasvanB <not.obelix@gmail.com>
2022-06-06 13:26:17 +0300

    tested query with variables

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,17 +1,35 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
-import { useParams } from 'react-router-dom';
+import { useContext, useState, useEffect, Fragment } from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "@apollo/client/core";
+import { useQuery } from "@apollo/client/react";
+import ProductCard from "../../components/product-card/product-card.component";
 
-import ProductCard from '../../components/product-card/product-card.component';
+import { CategoriesContext } from "../../contexts/categories.context";
 
-import { CategoriesContext } from '../../contexts/categories.context';
-
-import { CategoryContainer, Title } from './category.styles';
+import { CategoryContainer, Title } from "./category.styles";
+const GET_CATEGORY = gql`
+  query ($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 
 const Category = () => {
   const { category } = useParams();
   const { categoriesMap } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
-
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: { title: category },
+  });
+  console.log(data);
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
